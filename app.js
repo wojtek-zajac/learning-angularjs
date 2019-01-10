9fcecedacedd3bc138447bb660d2542215603d45
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-01-10 10:15:20 +0100

    Add user controller

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ myApp.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.text = 'Hello, Angular fanatic.';
 
 }]);
+
+myApp.controller('UserCtrl', ['$scope', function ($scope) {
+
+    // Let's namespace the user details
+    // Also great for DOM visual aids too
+    $scope.user = {};
+    $scope.user.details = {
+        "username": "Wojciech Zając",
+        "id": "553312"
+    };
+
+}])
