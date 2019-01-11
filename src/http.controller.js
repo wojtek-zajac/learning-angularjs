e15edc1c4686fc8e562edc1b6da24bf159040538
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-01-11 17:09:04 +0100

    Get an HHTP request and bind JSON

---
+++ src/http.controller.js
@@ -0,0 +1,17 @@
+myApp.controller('HttpCtrl', ['$scope', '$http', function ($scope, $http) {
+
+    $http({
+        method: 'GET',
+        url: 'https://api.smartrecruiters.com/v1/companies/PIQC/postings'
+    })
+    .then(function successCallback(response) {
+        
+        console.log(response.data.content);
+        $scope.postings = response.data.content;
+
+    }, function errorCallback(response) {
+        
+        alert('Something went wrong...');
+        console.log(response);
+    })
+}]);
