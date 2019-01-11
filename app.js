f25b6a143beb9add09c7047abc3444c25b235acd
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-01-11 14:33:10 +0100

    Add filter with ng-repeat

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ myApp.controller('UserCtrl', ['$scope', function ($scope) {
 
 }]);
 
+myApp.controller('FilterCtrl', ['$scope', function ($scope) {
+    
+    $scope.numbers = [1, 2, 3, 6, 10, 22, 33, 44, 55, 66, 67, 68, 88, 100, 101];
+    $scope.lowerBound = null;
+
+    $scope.greaterThanNum = function (i) {
+        return i > $scope.lowerBound;
+    };
+}]);
+
 myApp.directive('customButton', function () {
     return {
         restrict: 'AE',
