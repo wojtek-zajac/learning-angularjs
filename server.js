934c4338eaa0d3455df4fc6a0384a870c877bd59
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-01-16 00:10:24 +0100

    Serve static files with express and route postings

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/postings', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/postings.html'));
+});
+
+const server = app.listen(8080, () => {
+    console.log(`App is running on port ${server.address().port}`);
+});
